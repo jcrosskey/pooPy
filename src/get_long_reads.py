974437d3dcdbc1d293be59e71fada6e804fdcc74
JJ Chai <chaij@umail.iu.edu>
2015-06-03 12:16:27 -0400

    now also takes fastq as input format

--- src/get_long_reads.py
+++ src/get_long_reads.py
@@ -5,17 +5,53 @@ import sys
 import os
 import re 
 ## =================================================================
+## Function: getRead
+## =================================================================
+def getReadFromFasta(fin):
+    output = ""
+    for line in fin:
+        if line.strip("\n").strip() != "":
+            if line[0] != ">":
+                output += line
+            else:
+                if output != "":
+                    yield output
+                output = line
+    yield output
+
+def getReadFromFastq(fin):
+    output = ""
+    count = 0
+    for line in fin:
+        if count != 4:
+            output += line
+            count = count + 1
+        else:
+            yield output
+            output = line
+            count = 1
+    yield output
+
+## =================================================================
 ## Find length of all sequences in a fasta file
 ## =================================================================
-def get_long_reads(fastaFile,outFile=None,cutoff=1000):
-    ''' Extract sequences longer than the specified cutoff from fastaFile
-        Input:  fastaFile - file including all the sequences to investigate
+def get_long_reads(inputFile,outFile=None,cutoff=1000):
+    ''' Extract sequences longer than the specified cutoff from inputFile
+        Input:  inputFile - file including all the sequences to investigate
                 outFile - output file with long sequences
                 cutoff - length cutoff
         Output: outFile
     '''
-    if not os.path.exists(fastaFile):
-        sys.stderr.write("fastaFile {} does not exist!\n".format(fastaFile))
+    fileExt = inputFile.split(".")[-1] # file type, fasta or fastq, autodetect
+    if fileExt in ["fa", "Fa", "fasta", "Fasta"]:  # either fa or fq
+        fileType = "fasta" 
+    elif fileExt in ["fq", "Fq", "fastq", "Fastq"]:  # either fa or fq
+        fileType = "fastq" 
+    else:
+        sys.stderr.write("File type is not correct...\n") 
+
+    if not os.path.exists(inputFile):
+        sys.stderr.write("input File {} does not exist!\n".format(inputFile))
         return -1
     if outFile is None:
         outFile = os.getcwd()  + '/long_sequences.fasta'
@@ -23,36 +59,28 @@ def get_long_reads(fastaFile,outFile=None,cutoff=1000):
     if not os.path.exists(outDir):
         os.makedirs(outDir)
         
-    fasta = open(fastaFile,'r') 
-    myout = open(outFile,'w')
-    seqLen = 0 # initialize to 0
-    seq = ''
-    seqID = ''
-    seqNum = 0
-    with open(fastaFile,'r') as fasta:
-        for line in fasta:
-            if line[0] == '>':
-                if seqLen >= cutoff:
-                    seqNum += 1
-                    myout.write(">{}\n{}\n".format(seqID, seq))
-                # reset sequence length, and sequence bases
-                seqLen = 0
-                seqID = line.strip()[1:]
-                seq = ''
-            else:
-                seq += line.strip("\n")
-                seqLen += len(line.strip('\n'))
-    if seqLen >= cutoff:
-        seqNum += 1
-        myout.write("{}\n{}".format(seqID, seq))
-
-    myout.close()
-    sys.stdout.write("Total number of sequences longer than {} is {}\n".format(cutoff, seqNum))
+    fout = open(outFile, 'w')
+    readCount = 0
+    with open(inputFile,'r') as fin:
+        if fileType == "fastq":
+            for readLines in getReadFromFastq(fin):
+                seq_len = len(readLines.split("\n"[1]))
+                if seq_len >= cutoff:
+                    fout.write(readLines)
+                    readCount += 1
+        elif fileType == "fasta":
+            for readLines in getReadFromFasta(fin):
+                seq_len = sum(map(len, readLines.split("\n")[1:]))
+                if seq_len >= cutoff:
+                    fout.write(readLines)
+                    readCount += 1
+    fout.close()
+    sys.stderr.write("Total number of sequences longer than {} is {}\n".format(cutoff, readCount))
                     
 ## =================================================================
 ## argument parser
 ## =================================================================
-parser = argparse.ArgumentParser(description="Extract sequences longer than the specified cutoff from fastaFile",
+parser = argparse.ArgumentParser(description="Extract sequences longer than the specified cutoff from sequence File",
                                  prog = 'get_long_reads', #program name
                                  prefix_chars='-', # prefix for options
                                  fromfile_prefix_chars='@', # if options are read from file, '@args.txt'
@@ -62,11 +90,11 @@ parser = argparse.ArgumentParser(description="Extract sequences longer than the 
                                  )
 
 ## input files and directories
-parser.add_argument("-i","--in",help="input fasta file",dest='fastaFile',required=True)
+parser.add_argument("-i","--in",help="input file",dest='inputFile',required=True)
 parser.add_argument("-c","--cutoff",help="length cutoff",dest='cutoff',default=1000, type=int)
 
 ## output directory
-parser.add_argument("-o","--out",help="output fasta file",dest='outFile',default=None)
+parser.add_argument("-o","--out",help="output file",dest='outFile',default=None)
 
 ## =================================================================
 ## main function
@@ -75,7 +103,7 @@ def main(argv=None):
     if argv is None:
         args = parser.parse_args()
 
-    get_long_reads(args.fastaFile, args.outFile, args.cutoff)
+    get_long_reads(args.inputFile, args.outFile, args.cutoff)
 ##==============================================================
 ## call from command line (instead of interactively)
 ##==============================================================
